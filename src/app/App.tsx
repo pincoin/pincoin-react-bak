a5e817cf9dec1f9bdbf6e3795d573bbd0e339599
pincoin <pincoins@gmail.com>
2023-06-11 21:20:21 +0900

    navbar, aside

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,17 @@
 import React from "react"
 
 const App = () => {
-  return <h1 className="text-9xl underline">리액트 타입스크립트</h1>
+  return (
+    <div className="flex flex-col h-full">
+      {/*  display: flex; flex-direction: column; height: 100%; */}
+      <div className="bg-emerald-100">상단메뉴</div>
+      <div className="flex flex-1">
+        {/*  상단메뉴 아래 나머지 여백 모두 채움 */}
+        <div className="flex flex-none bg-red-200">좌측메뉴</div>
+        <div className="flex flex-1 bg-red-100">본문</div>
+      </div>
+    </div>
+  )
 }
 
 export default App
